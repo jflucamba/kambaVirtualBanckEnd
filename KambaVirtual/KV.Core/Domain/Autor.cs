59b68a648780fa663491524ebfb27f1837da2917
jflucamba <jflucamba@gmail.com>
2022-07-09 09:27:15 +0100

    Retomando o trabalho, com novo dinamismo.

---
+++ KambaVirtual/KV.Core/Domain/Autor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KV.Core.Domain
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
